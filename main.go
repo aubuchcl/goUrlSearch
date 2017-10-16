6b880e8300d14edcd4b51a0f4c49730d6a384ff8
Christopher Aubuchon <aubuchcl@gmail.com>
2017-10-16 01:21:45 -0700

    working sort bytes removed from slice

--- main.go
+++ main.go
@@ -28,54 +28,63 @@ func main() {
 
 	resp.Body.Read(bs)
 	fmt.Println(err)
-
-	stringSlice := stripResponse(string(bs))
-	charOnlySlice := charSlice(stringSlice)
-	charSliceSort := charSort(charOnlySlice)
-
-	if 12 == 23 {
-		fmt.Println(stringSlice)
-		fmt.Println(charSliceSort)
-	}
-	//fmt.Println(charOnlySlice)
-
-}
-
-func stripResponse(responseString string) []string {
-
+	innerHTML := strip.StripTags(string(bs))
 	//replace this with a regex if you have time.
-	innerHTML := strip.StripTags(responseString)
 	innerHTML = strings.Replace(innerHTML, "\n", "", -1)
 	innerHTML = strings.Replace(innerHTML, ",", "", -1)
 	innerHTML = strings.Replace(innerHTML, "\t", "", -1)
 	innerHTML = strings.Replace(innerHTML, ".", "", -1)
 	innerHTML = strings.Replace(innerHTML, ",", "", -1)
-	//innerHTML = strings.Replace(innerHTML, " ", "", -1)
+	innerHTML = strings.Replace(innerHTML, " ", "", -1)
+	var xyz []character
+	for _, c := range innerHTML {
+		if c == 0 {
+			continue
+		} else {
+			z := strings.Count(innerHTML, string(c))
+			//fmt.Println(reflect.TypeOf(c), string(c))
+			xyz = append(xyz, character{string(c), z})
 
-	htmlSlice := strings.Split(innerHTML, " ")
-	fmt.Println(htmlSlice)
-
-	return htmlSlice
-}
-
-//function that takes a slice and makes a slice of chars out of it.
-func charSlice(slc []string) []character {
-
-	var newCharSlice []character
-	for _, s := range slc {
-		for _, c := range string(s) {
-			newCharSlice = append(newCharSlice, character{string(c), 0})
 		}
 	}
+	//fmt.Println(xyz)
+	// stringSlice := stripResponse(innerHTML)
+	// charOnlySlice := charSlice(stringSlice)
+	charSliceSort := charSort(xyz)
+	fmt.Println(charSliceSort)
 
-	return newCharSlice
+	// if 12 == 23 {
+	// 	fmt.Println(stringSlice)
+	// 	fmt.Println(charSliceSort)
+	// 	fmt.Println(innerHTML)
+	// }
+	//fmt.Println(charOnlySlice)
+
 }
 
-//looking to compare newCharSlice[index].char
+// func stripResponse(responseString string) []string {
+// 	htmlSlice := strings.Split(responseString, " ")
+// 	return htmlSlice
+// }
+
+// //function that takes a slice and makes a slice of chars out of it.
+// func charSlice(slc []string) []character {
+
+// 	var newCharSlice []character
+// 	for _, s := range slc {
+// 		for _, c := range string(s) {
+// 			newCharSlice = append(newCharSlice, character{string(c), 0})
+// 		}
+// 	}
+
+// 	return newCharSlice
+// }
+
+// //looking to compare newCharSlice[index].char
 func charSort(slc []character) []character {
 	sort.SliceStable(slc, func(i, j int) bool {
-		return slc[i].char < slc[j].char
+		return slc[i].count > slc[j].count
 	})
-	fmt.Println("By Char:", slc)
+	//fmt.Println("By Char:", slc)
 	return slc
 }
